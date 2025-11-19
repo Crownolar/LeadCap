38a0add2669049214deb3ba28bb25ae0aaf6abf1
Crownolar <oriadeolamilekan500@gmail.com>
2025-11-19 15:50:22 +0100

    Theme mode

--- src/components/views/Dashboard.jsx
+++ src/components/views/Dashboard.jsx
@@ -68,7 +68,7 @@ const Dashboard = ({ theme, darkMode }) => {
   };
 
   return (
-    <div className="space-y-6 px-10">
+    <div className={`space-y-6 px-10 ${theme?.text}`}>
       <div
         className={`grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 ${theme?.text}`}
       >
